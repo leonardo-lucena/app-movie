fe971a3bccc9976562bee83d4b80c03170936806
Ayslan Câmara de Moura Silveira <ayslan.silveira@jfpe.jus.br>
2023-11-21 15:38:52 -0300

    request de dados
    arquivo para usar de base para função de obtenção dos dados do filme.

---
+++ control/filme_info_request.dart
@@ -0,0 +1,54 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<Map<String, dynamic>> getFilmePorNome(String nome) async {
+  const String apiKey = '39d0df6124fbb8624eb376008182c94e';
+  final String url =
+      'https://api.themoviedb.org/3/search/movie?query=$nome&api_key=$apiKey&language=pt-BR';
+  final http.Response response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> json = jsonDecode(response.body);
+    final Map<String, dynamic> resultado = json['results'][0];
+
+    // Obtenha o ID do filme
+    final int filmeId = resultado['id'];
+
+    // Crie uma URL para a solicitação de classificação
+    final String urlRating =
+        'https://api.themoviedb.org/3/movie/$filmeId?api_key=$apiKey&language=pt-BR';
+
+    // Faça a solicitação de classificação
+    final http.Response responseRating = await http.get(Uri.parse(urlRating));
+
+    // Decodifique a resposta
+    final Map<String, dynamic> jsonRating = jsonDecode(responseRating.body);
+
+    // Obtenha a classificação do filme
+    final double rating = jsonRating['vote_average'];
+
+    return {
+      'thumbnail': resultado['poster_path'],
+      'nome': resultado['title'],
+      'ano_de_lancamento': resultado['release_date'],
+      'rating': rating,
+      'descricao': resultado['overview'],
+    };
+  } else {
+    print('Erro ao fazer a solicitação: ${response.statusCode}');
+    return {};
+  }
+}
+
+void main() async {
+  const String nomeFilme = 'The Batman';
+  final Map<String, dynamic> filme = await getFilmePorNome(nomeFilme);
+
+  //   https://image.tmdb.org/t/p/original/
+  //usar quando passar a uri do thumb no app
+  print('Thumbnail: ${filme['thumbnail']}');
+  print('Nome: ${filme['nome']}');
+  print('Ano de lançamento: ${filme['ano_de_lancamento']}');
+  print('Rating: ${filme['rating']} de 10');
+  print('Descrição: ${filme['descricao']}');
+}
